da9c2d40be3399fd72d1c374ea443f967dc4c4b5
Sage Elliott <wildtroll@gmail.com>
2016-02-01 07:55:06 -0800

    Feat: Add conditional to catalog.php to check catagory from URL. Change pageTitle based on cta

--- catalog.php
+++ catalog.php
@@ -1,11 +1,21 @@
 <?php
-$pageTitle = "Full Catalog"
- ?>
+$pageTitle = "Full Catalog";
 
-<?php include("inc/header.php"); ?>
+if (isset($_GET["cat"])) {
+    if ($_GET['cat'] == "books") {
+        $pageTitle = 'Books';
+    } else if ($_GET['cat'] == "movies") {
+        $pageTitle = 'Movies';
+    } else if ($_GET['cat'] == 'music') {
+        $pageTitle = 'Music';
+    }
+}
+
+
+include("inc/header.php"); ?>
 
 <div class="section page">
-    <h1>Full Catalog</h1>
+    <h1><?php echo $pageTitle; ?></h1>
 </div>
 
 <?php include("inc/footer.php"); ?>
--- inc/header.php
+++ inc/header.php
@@ -14,9 +14,9 @@
             <h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
 
             <ul class="nav">
-                <li class="books"><a href="catalog.php">Books</a></li>
-                <li class="movies"><a href="catalog.php">Movies</a></li>
-                <li class="music"><a href="catalog.php">Music</a></li>
+                <li class="books"><a href="catalog.php?cat=books">Books</a></li>
+                <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
+                <li class="music"><a href="catalog.php?cat=music">Music</a></li>
                 <li class="suggest"><a href="suggest.php">Suggest</a></li>
             </ul>
 
