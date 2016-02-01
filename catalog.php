c3bbb3392bd33b7a2f77a3bc3d6859c5133942cd
Sage Elliott <wildtroll@gmail.com>
2016-02-01 07:36:29 -0800

    Add Catalog.php and link in all media links

--- catalog.php
+++ catalog.php
@@ -1,11 +1,7 @@
-<?php
-$pageTitle = "Suggest a Media Item"
- ?>
-
 <?php include("inc/header.php"); ?>
 
 <div class="section page">
-    <h1>Suggest a media Item</h1>
+    <h1>Full Catalog</h1>
 </div>
 
 <?php include("inc/footer.php"); ?>
--- inc/header.php
+++ inc/header.php
@@ -14,9 +14,9 @@
             <h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
 
             <ul class="nav">
-                <li class="books"><a href="#">Books</a></li>
-                <li class="movies"><a href="#">Movies</a></li>
-                <li class="music"><a href="#">Music</a></li>
+                <li class="books"><a href="catalog.php">Books</a></li>
+                <li class="movies"><a href="catalog.php">Movies</a></li>
+                <li class="music"><a href="catalog.php">Music</a></li>
                 <li class="suggest"><a href="suggest.php">Suggest</a></li>
             </ul>
 
