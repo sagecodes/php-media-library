33a4ab41460088e22976c268b1fbf2e5c921d4cf
Sage Elliott <wildtroll@gmail.com>
2016-02-01 10:09:06 -0800

    Feat: Create array with items and display item in a Ulist using foreach loop

--- catalog.php
+++ catalog.php
@@ -1,4 +1,9 @@
 <?php
+$catalog = array("Design Patterns",
+                 "Forrest Gump",
+                 "Beethoven"
+                );
+
 $pageTitle = "Full Catalog";
 $section = null;
 
@@ -18,8 +23,19 @@ if (isset($_GET["cat"])) {
 
 include("inc/header.php"); ?>
 
-<div class="section page">
-    <h1><?php echo $pageTitle; ?></h1>
+<div class="section catalog page">
+    <div class="wrapper">
+        <h1><?php echo $pageTitle; ?></h1>
+
+        <ul>
+            <?php foreach($catalog as $item) {
+                 echo "<li>" . $item . "</li>";
+            }
+            ?>
+
+        </ul>
+
+    </div>
 </div>
 
 <?php include("inc/footer.php"); ?>
